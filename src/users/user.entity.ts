5e75a006770a63c8a3fdf241832cf1596db35f8c
Ahmed Tarek <ahmed.tarek1244@gmail.com>
2024-07-02 20:18:14 +0300

    add update user feature'

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  id: Number;
+  id: number;
 
   @Column()
   email: string;
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,36 +1,52 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
-import { DataSource, Repository } from 'typeorm';
+import { DataSource, ILike, Repository } from 'typeorm';
+import { emit } from 'process';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private userRepo: Repository<User>,
-  private dataSource: DataSource) {}
+  constructor(
+    @InjectRepository(User) private userRepo: Repository<User>,
+    private dataSource: DataSource,
+  ) {}
   async createUser(email: string, password: string) {
     /* Transactions are useful for ensuring that a series of
     database operations either all succeed or
     all fail, maintaining data integrity.*/
 
-    
-    const queryRunner=this.dataSource.createQueryRunner();
+    const queryRunner = this.dataSource.createQueryRunner();
 
     await queryRunner.connect();
     await queryRunner.startTransaction();
-    try{
-
-        const user=this.userRepo.create({email,password});
-        const savedUser=await queryRunner.manager.save(user);
-        await queryRunner.commitTransaction();
-        return savedUser;
-    }catch(err){
-        console.log(err);
-        await queryRunner.rollbackTransaction();
-        throw err;
-    }finally{
-        await queryRunner.release();
-
+    try {
+      const user = this.userRepo.create({ email, password });
+      const savedUser = await queryRunner.manager.save(user);
+      await queryRunner.commitTransaction();
+      return savedUser;
+    } catch (err) {
+      console.log(err);
+      await queryRunner.rollbackTransaction();
+      throw err;
+    } finally {
+      await queryRunner.release();
     }
-    
   }
+
+  findOne(id: number) {
+    return this.userRepo.findOneBy({ id });
+  }
+
+  find(email: string) {
+    return this.userRepo.find({
+      where: {
+        email: ILike(email),
+      },
+    });
+  }
+  async update(id: number, attrs: Partial<User>) :Promise<User|null>{
+    const user = await this.userRepo.findOneBy({ id });
+    Object.assign(user, attrs);
+    return this.userRepo.save(user);
+   }
 }
