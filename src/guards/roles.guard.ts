f27354192b69e1454d09eab9877b0cdf0eb62db9
Ahmed-T-Fathy <ahmed.tarek1244@gmail.com>
2024-07-16 15:50:40 +0300

    work on auth

--- src/guards/roles.guard.ts
+++ src/guards/roles.guard.ts
@@ -6,24 +6,26 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class RolesGuard implements CanActivate {
   // The `Reflector` service is used to read the metadata attached to the route handler.
-  constructor(private reflector: Reflector) {}
+  constructor(private reflector:Reflector){}
 
   // `canActivate` method is invoked to decide if the current request is allowed to proceed.
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     // Get the roles metadata set by the `Roles` decorator.
-    const roles = this.reflector.get<string[]>('roles', context.getHandler());
-    if (!roles) {
+    const roles=this.reflector.get<string>('roles',context.getHandler())
+    if(!roles){
       // If no roles metadata is found, allow the request to proceed.
       return true;
     }
+
     // Extract the request object from the execution context.
-    const request = context.switchToHttp().getRequest();
+    const request=context.switchToHttp().getRequest();
+
     // Get the user object from the request (assuming it's attached by some authentication middleware).
-    const user = request.user;
+    const user=request.user;
 
     // Check if the user's role is included in the allowed roles.
-    return roles.some(role => user.role.includes(role));
+      return roles.includes(user.role);
   }
 }
--- src/users/decorators/roles.decorators.ts
+++ src/users/decorators/roles.decorators.ts
@@ -1,4 +1,7 @@
 import { SetMetadata } from '@nestjs/common';
 
 // Custom decorator named `Roles` that accepts a list of roles as arguments.
-export const Roles = (...roles: string[]) => SetMetadata('roles', roles);
+export const Roles = (...roles: string[]) => {
+  console.log('Hey iam in seting meta data');
+  SetMetadata('roles', roles);
+};
